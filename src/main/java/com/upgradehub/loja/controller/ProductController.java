a01bbfe1cbf32caa311655c4bd8fc5c65eb09a82
alvarotaveirapinto <alvarotaveirapinto@gmail.com>
2021-10-21 19:26:56 +0100

    changes made again

--- src/main/java/com/upgradehub/loja/controller/ProductController.java
+++ src/main/java/com/upgradehub/loja/controller/ProductController.java
@@ -1,11 +1,17 @@
 package com.upgradehub.loja.controller;
 
+import com.upgradehub.loja.model.Product;
 import com.upgradehub.loja.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
     @Autowired
     private ProductService productService;
+
+
 }
--- src/main/java/com/upgradehub/loja/service/ProductService.java
+++ src/main/java/com/upgradehub/loja/service/ProductService.java
@@ -1,11 +1,16 @@
 package com.upgradehub.loja.service;
 
+import com.upgradehub.loja.model.Product;
 import com.upgradehub.loja.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
+
+
 }
